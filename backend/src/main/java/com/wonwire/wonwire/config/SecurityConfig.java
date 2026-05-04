0c37b76227797cfab3a5c494ee3b7fa0c73e7595
tommydll <thomasdaulle.pro@gmail.com>
2026-05-04 18:30:46 +0900

    fix(backend: CORS): add customizer withDefaults to make spring security uses our own CORS config

--- backend/src/main/java/com/wonwire/wonwire/config/SecurityConfig.java
+++ backend/src/main/java/com/wonwire/wonwire/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
